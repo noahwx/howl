304b2a010c56542cf483373a5f7741d450bd6807
noah <noahfontenette@outlook.com>
2022-11-26 22:56:29 -0600

    A0.0.001

---
+++ supabase-react/src/Dashboard.js
@@ -0,0 +1,14 @@
+import { supabase } from './supabaseClient'
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            <div>
+                
+            </div>
+        </div>
+    );
+}
+ 
+export default Dashboard;
